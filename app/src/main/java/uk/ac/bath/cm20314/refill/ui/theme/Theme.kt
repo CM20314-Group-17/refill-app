82831bc9c59cdfb80ec333b237c7ef441a6f5b66
unknown <80782387+aalister@users.noreply.github.com>
2023-02-15 15:13:30 +0000

    Updated theme
    Added custom navigation bar colour. Removed dynamic colour. Changed Material 3 version to 1.0.1.

--- app/src/main/java/uk/ac/bath/cm20314/refill/ui/theme/Theme.kt
+++ app/src/main/java/uk/ac/bath/cm20314/refill/ui/theme/Theme.kt
@@ -5,64 +5,55 @@ import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
+private val DarkColourScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
-private val LightColorScheme = lightColorScheme(
+private val LightColourScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
+/** Displays the content using the app's theme. */
 @Composable
 fun RefillTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colours = if (darkTheme) DarkColourScheme else LightColourScheme
     val view = LocalView.current
+
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            val window = (view.context as Activity).window
+            val insets = WindowCompat.getInsetsController(window, view)
+
+            // Change status bar colours.
+            window.statusBarColor = colours.background.toArgb()
+            insets.isAppearanceLightStatusBars = !darkTheme
+
+            // Change navigation bar colours.
+            // Only works on API level 26 or higher.
+            if (Build.VERSION.SDK_INT >= 26) {
+                window.navigationBarColor = colours.background.toArgb()
+                insets.isAppearanceLightNavigationBars = !darkTheme
+            }
         }
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
+        colorScheme = colours,
+        content = content,
+        typography = Typography
     )
 }
