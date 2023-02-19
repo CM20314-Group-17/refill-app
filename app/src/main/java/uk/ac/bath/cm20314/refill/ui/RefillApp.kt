71031138fd2e06a9e2d1f3cfe30a6b9cab64ae11
unknown <80782387+aalister@users.noreply.github.com>
2023-02-19 19:11:28 +0000

    Added selected theme storage
    Added a preferences data store that saves the selected theme. The theme will be selectable from the settings screen when it is added.

--- app/src/main/java/uk/ac/bath/cm20314/refill/ui/RefillApp.kt
+++ app/src/main/java/uk/ac/bath/cm20314/refill/ui/RefillApp.kt
@@ -1,24 +1,39 @@
 package uk.ac.bath.cm20314.refill.ui
 
+import android.content.Context
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.map
 import uk.ac.bath.cm20314.refill.ui.theme.RefillTheme
+
+val Context.dataStore by preferencesDataStore(name = "preferences")
 
 /** Contains the app's entire user interface. */
 @Composable
 fun RefillApp() {
-    RefillTheme {
+    val darkTheme by rememberDarkTheme()
+
+    RefillTheme(darkTheme = darkTheme ?: isSystemInDarkTheme()) {
         Surface(modifier = Modifier.fillMaxSize()) {
             NavGraph()
         }
     }
 }
 
-@Preview
+/** Returns the selected theme, or null if the user has not selected a theme. */
 @Composable
-fun RefillAppPreview() {
-    RefillApp()
+fun rememberDarkTheme(): State<Boolean?> {
+    val context = LocalContext.current
+
+    return remember {
+        context.dataStore.data.map { preferences ->
+            preferences[booleanPreferencesKey(name = "darkTheme")]
+        }
+    }.collectAsState(initial = null)
 }
