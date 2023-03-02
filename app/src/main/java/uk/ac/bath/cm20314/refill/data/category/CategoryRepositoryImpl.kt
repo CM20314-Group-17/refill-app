739d8abbc4288de1b74afc1155620b6ba53c5d22
unknown <80782387+aalister@users.noreply.github.com>
2023-03-02 18:44:31 +0000

    Updated category repository
    Fixed crash when going to the category screen

--- app/src/main/java/uk/ac/bath/cm20314/refill/data/category/CategoryRepositoryImpl.kt
+++ app/src/main/java/uk/ac/bath/cm20314/refill/data/category/CategoryRepositoryImpl.kt
@@ -1,31 +1,35 @@
 package uk.ac.bath.cm20314.refill.data.category
 
+import java.util.UUID
+
 object CategoryRepositoryImpl : CategoryRepository {
 
+    private var data = mutableListOf(
+        Category(id = "1", name = "Category 1", itemCount = 1, isUpdated = true),
+        Category(id = "2", name = "Category 2", itemCount = 2, isUpdated = true),
+        Category(id = "3", name = "Category 3", itemCount = 3, isUpdated = true)
+    )
+
     override suspend fun getCategories(): List<Category> {
-        // TODO: Replace with categories retrieved from the database.
-        return listOf(
-            Category(id = "test", name = "Dried Fruits", itemCount = 9),
-            Category(id = "test", name = "Pasta", itemCount = 8),
-            Category(id = "test", name = "Nuts & Seeds", itemCount = 9),
-            Category(id = "test", name = "Rice", itemCount = 5),
-            Category(id = "test", name = "Beans", itemCount = 4)
-        )
+        return data.map { it.copy() }
     }
 
     override suspend fun getCategory(categoryId: String): Category? {
-        TODO()
+        return data.find { it.id == categoryId }
     }
 
     override suspend fun updateCategory(category: Category) {
-        TODO()
+        getCategory(category.id)?.apply {
+            name = category.name
+            isUpdated = category.isUpdated
+        }
     }
 
     override suspend fun createCategory(name: String): Category {
-        TODO()
+        return Category(UUID.randomUUID().toString(), name, itemCount = 0).also { data.add(it) }
     }
 
     override suspend fun deleteCategory(categoryId: String) {
-
+        data.removeIf { it.id == categoryId }
     }
 }
